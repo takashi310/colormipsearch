49e48291b7f84f4487b24e9af07011539b473b07
Cristian Goina <goinac@hhmi.org>
2020-04-27 20:52:03 -0400

    changed cache expiration

--- src/main/java/org/janelia/colormipsearch/CachedMIPsUtils.java
+++ src/main/java/org/janelia/colormipsearch/CachedMIPsUtils.java
@@ -15,7 +15,7 @@ class CachedMIPsUtils {
 
     private static final LoadingCache<MIPInfo, MIPImage> MIP_IMAGES_CACHE = CacheBuilder.newBuilder()
             .maximumSize(200000L)
-            .expireAfterAccess(Duration.ofMinutes(30))
+            .expireAfterAccess(Duration.ofMinutes(120))
             .concurrencyLevel(16)
             .build(new CacheLoader<MIPInfo, MIPImage>() {
                 @Override
