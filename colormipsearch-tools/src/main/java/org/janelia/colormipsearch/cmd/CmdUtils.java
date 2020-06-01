47d355754d3b4573ec6484850203f9647468b112
Cristian Goina <goinac@hhmi.org>
2020-06-01 12:23:24 -0400

    trying workstealing with parallelization option

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CmdUtils.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CmdUtils.java
@@ -21,12 +21,7 @@ class CmdUtils {
         if (args.cdsConcurrency > 0) {
             LOG.info("Create a thread pool with {} worker threads ({} available processors for workstealing pool)",
                     args.cdsConcurrency, Runtime.getRuntime().availableProcessors());
-            return Executors.newFixedThreadPool(
-                    args.cdsConcurrency,
-                    new ThreadFactoryBuilder()
-                            .setNameFormat("CDSRUNNER-%d")
-                            .setDaemon(true)
-                            .build());
+            return Executors.newWorkStealingPool(args.cdsConcurrency);
         } else {
             LOG.info("Create a workstealing pool with {} worker threads", Runtime.getRuntime().availableProcessors());
             return Executors.newWorkStealingPool();
