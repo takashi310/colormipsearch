9bced04aa5c445a658cd3d182108d4d517e4386b
Cristian Goina <goinac@janelia.hhmi.org>
2020-06-05 22:29:36 -0400

    set GC and LOG opts

--- colormipsearch-tools/src/main/scripts/copyGAScores2ResultsByLib.sh
+++ colormipsearch-tools/src/main/scripts/copyGAScores2ResultsByLib.sh
@@ -4,7 +4,16 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 source ${SCRIPT_DIR}/cdsparams.sh
 
-java -Xms360G -Xmx360G \
+if [ -n "${LOGFILE}" ] && [ -f "${LOGFILE}" ] ; then
+    echo "Using Log config: ${LOGFILE}"
+    LOG_OPTS="-Dlog4j.configuration=file://${LOGFILE}"
+else
+    LOG_OPTS=""
+fi
+
+JAVA_EXEC=${JAVA_EXEC:java}
+
+${JAVA_EXEC} ${GC_OPTS} ${LOG_OPTS} -Xms460G -Xmx460G \
   -jar target/colormipsearch-1.1-jar-with-dependencies.jar \
   gradientScoresFromMatchedResults \
   -revd ${CDGAS_RESULTS_DIR}/${RESULTS_SUBDIR_FOR_MASKS} \
