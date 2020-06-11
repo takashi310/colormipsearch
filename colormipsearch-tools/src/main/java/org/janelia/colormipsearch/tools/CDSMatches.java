f254ec5ed59ba1e1bd580e155c1d602b2e2b3e9c
Cristian Goina <goinac@hhmi.org>
2020-06-11 18:50:29 -0400

    ignore isEmpty

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/tools/CDSMatches.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/tools/CDSMatches.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.apache.commons.collections4.CollectionUtils;
@@ -64,6 +65,7 @@ public class CDSMatches extends Results<List<ColorMIPSearchMatchMetadata>> {
         }
     }
 
+    @JsonIgnore
     public boolean isEmpty() {
         return CollectionUtils.isEmpty(results);
     }
