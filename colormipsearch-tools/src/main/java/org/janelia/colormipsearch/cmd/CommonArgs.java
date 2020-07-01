b279d86f82638cf5369639098422267d39033eea
Cristian Goina <goinac@hhmi.org>
2020-07-01 14:45:18 -0400

    changed some cmdline flags

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CommonArgs.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CommonArgs.java
@@ -6,9 +6,9 @@ class CommonArgs {
     @Parameter(names = {"--outputDir", "-od"}, description = "Output directory")
     String outputDir;
 
-    @Parameter(names = "-no-pretty-print", description = "Do not pretty print the results", arity = 0)
+    @Parameter(names = "--no-pretty-print", description = "Do not pretty print the results", arity = 0)
     boolean noPrettyPrint = false;
 
-    @Parameter(names = "-h", description = "Display the help message", help = true, arity = 0)
+    @Parameter(names = {"--help", "-h"}, description = "Display the help message", help = true, arity = 0)
     boolean displayHelpMessage = false;
 }
