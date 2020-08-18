28eacc0c1105211b3f4f0363c9aaf53a95e44ec4
Cristian Goina <goinac@hhmi.org>
2020-08-18 17:59:03 -0400

    removed parallelization when reading reverse matches

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/UpdateGradientScoresFromReverseSearchResultsCmd.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/UpdateGradientScoresFromReverseSearchResultsCmd.java
@@ -185,7 +185,7 @@ class UpdateGradientScoresFromReverseSearchResultsCmd extends AbstractCmd {
     }
 
     private Map<String, List<ColorMIPSearchMatchMetadata>> readMatchIdResults(Set<String> ids, Set<String> matchIds, LoadingCache<String, List<ColorMIPSearchMatchMetadata>> reverseResultsCache) {
-        return matchIds.stream().parallel()
+        return matchIds.stream()
                 .flatMap(matchId -> {
                     try {
                         return streamMatchResultsWithGradientScore(reverseResultsCache.get(matchId), ids);
