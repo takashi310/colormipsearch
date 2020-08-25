a2f376d8fa1b3218dd03c79bd0657588988fb7be
Cristian Goina <goinac@hhmi.org>
2020-08-25 11:42:13 -0400

    set concurrency options

--- colormipsearch-tools/src/main/scripts/submitUpdateRevGAJob.sh
+++ colormipsearch-tools/src/main/scripts/submitUpdateRevGAJob.sh
@@ -7,6 +7,14 @@ function updateRevGAJob {
     CDGA_OUTPUT_PARAM=$1
     shift
 
+    REQUESTED_CORES=$((${CORES_RESOURCE:-0}))
+    CPU_RESERVE=$((${CPU_RESERVE:-1}))
+    CONCURRENCY=$((2 * REQUESTED_CORES - CPU_RESERVE))
+    if [ ${CONCURRENCY} -lt 0 ] ; then
+        CONCURRENCY=0
+    fi
+    CONCURRENCY_OPTS="--cdsConcurrency ${CONCURRENCY}"
+
     MEM_OPTS="-Xmx${MEM_RESOURCE}G -Xms${MEM_RESOURCE}G"
     if [ -n "${LOGCONFIGFILE}" ] && [ -f "${LOGCONFIGFILE}" ] ; then
         echo "Using Log config: ${LOGCONFIGFILE}"
@@ -25,6 +33,7 @@ function updateRevGAJob {
          -jar ${CDS_JAR} \
          --cacheSize ${MIPS_CACHE_SIZE} --cacheExpirationInSeconds ${MIPS_CACHE_EXPIRATION} \
          gradientScoresFromMatchedResults \
+        ${CONCURRENCY_OPTS} \
          -revd ${CDGAS_RESULTS_DIR}/${RESULTS_SUBDIR_FOR_MASKS} \
          -rd ${CDGA_INPUT_PARAM} \
          -od ${CDGA_OUTPUT_PARAM} \
