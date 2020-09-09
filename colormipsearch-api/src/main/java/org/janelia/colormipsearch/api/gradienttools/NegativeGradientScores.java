70ad727e49b057b0b755e9fd8e991081d91fbd09
Cristian Goina <goinac@hhmi.org>
2020-09-09 15:04:54 -0400

    set version  to 2.5.0

--- colormipsearch-api/src/main/java/org/janelia/colormipsearch/api/gradienttools/NegativeGradientScores.java
+++ colormipsearch-api/src/main/java/org/janelia/colormipsearch/api/gradienttools/NegativeGradientScores.java
@@ -1,5 +1,7 @@
 package org.janelia.colormipsearch.api.gradienttools;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 public class NegativeGradientScores {
     public final long gradientAreaGap;
     public final long highExpressionArea;
@@ -11,5 +13,13 @@ public class NegativeGradientScores {
 
     public long getCumulatedScore() {
         return gradientAreaGap + highExpressionArea / 3;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("gradientAreaGap", gradientAreaGap)
+                .append("highExpressionArea", highExpressionArea)
+                .toString();
     }
 }
