43b45e1c9754e8a6010e4aa471d2e1b1185a0118
Cristian Goina <goinac@hhmi.org>
2021-06-29 09:54:08 -0400

    removed merge method since it's not needed

--- colormipsearch-api/src/main/java/org/janelia/colormipsearch/api/pppsearch/PPPUtils.java
+++ colormipsearch-api/src/main/java/org/janelia/colormipsearch/api/pppsearch/PPPUtils.java
@@ -3,76 +3,15 @@ package org.janelia.colormipsearch.api.pppsearch;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectWriter;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PPPUtils {
     private static final Logger LOG = LoggerFactory.getLogger(PPPUtils.class);
-
-    public static List<SourcePPPMatch> mergeMatches(List<SourcePPPMatch> l1, List<SourcePPPMatch> l2) {
-        return Stream.concat(stream(l1), stream(l2))
-                .collect(Collectors.groupingBy(
-                        pppMatch -> ImmutablePair.of(pppMatch.getSourceEmName(), pppMatch.getSourceLmName()),
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                listOfPPPMatches -> listOfPPPMatches.stream()
-                                        .reduce(PPPUtils::mergePPPMatch))))
-                .values().stream()
-                .map(om -> om.orElse(null))
-                .filter(m -> m != null)
-                .collect(Collectors.toList())
-                ;
-    }
-
-    private static SourcePPPMatch mergePPPMatch(SourcePPPMatch m1, SourcePPPMatch m2) {
-        SourcePPPMatch m = new SourcePPPMatch();
-        m.setSourceEmName(m1.getSourceEmName());
-        m.setNeuronName(m1.getNeuronName());
-        m.setNeuronType(m1.getNeuronType());
-        m.setNeuronInstance(m1.getNeuronInstance());
-        m.setSourceLmName(m1.getSourceLmName());
-        m.setSampleName(m1.getSampleName());
-        m.setLineName(m1.getLineName());
-        m.setSlideCode(m1.getSlideCode());
-        m.setObjective(m1.getObjective());
-        m.setAlignmentSpace(m1.getAlignmentSpace());
-        m.setCoverageScore(m1.getCoverageScore());
-        m.setAggregateCoverage(m1.getAggregateCoverage());
-        m.setMirrored(m1.getMirrored());
-        m.setSkeletonMatches(mergeSkeletonMatches(m1.getSkeletonMatches(), m2.getSkeletonMatches()));
-        if (m1.hasEmPPPRank()) {
-            m.setEmPPPRank(m1.getEmPPPRank());
-        } else if (m2.hasEmPPPRank()) {
-            m.setEmPPPRank(m2.getEmPPPRank());
-        }
-        return m;
-    }
-
-    private static List<SkeletonMatch> mergeSkeletonMatches(List<SkeletonMatch> l1, List<SkeletonMatch> l2) {
-        return mergeSkeletonsStream(Stream.concat(stream(l1), stream(l2)));
-    }
-
-    private static List<SkeletonMatch> mergeSkeletonsStream(Stream<SkeletonMatch> matches) {
-        return matches.collect(Collectors.groupingBy(SkeletonMatch::getId, Collectors.toList()))
-                .values().stream()
-                .map(e -> e.stream().max(Comparator.comparingDouble(SkeletonMatch::getNblastScore)).orElse(null))
-                .filter(e -> e != null)
-                .sorted(Comparator.comparingDouble(SkeletonMatch::getNblastScore).reversed())
-                .collect(Collectors.toList());
-    }
-
-    private static <E> Stream<E> stream(List<E> l) {
-        return CollectionUtils.isEmpty(l) ? Stream.empty() : l.stream();
-    }
 
     public static void writePPPMatchesToJSONFile(PPPMatches pppMatches, File f, ObjectWriter objectWriter) {
         try {
@@ -88,7 +27,6 @@ public class PPPUtils {
             LOG.error("Error writing CDS results to json file {}", f, e);
             throw new UncheckedIOException(e);
         }
-
     }
 
 }
