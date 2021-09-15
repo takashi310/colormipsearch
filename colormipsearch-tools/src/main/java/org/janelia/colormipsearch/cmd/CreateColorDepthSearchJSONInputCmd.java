89d52669fe6b129735eeb50838b9bea281237ded
Cristian Goina <goinac@hhmi.org>
2021-09-15 13:01:48 -0400

    debug

--- colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CreateColorDepthSearchJSONInputCmd.java
+++ colormipsearch-tools/src/main/java/org/janelia/colormipsearch/cmd/CreateColorDepthSearchJSONInputCmd.java
@@ -491,6 +491,9 @@ public class CreateColorDepthSearchJSONInputCmd extends AbstractCmd {
                 LOG.info("Process {} entries from {} to {} out of {}", cdmipsPage.size(), pageOffset, pageOffset + pageSize, cdmsCount);
                 cdmipsPage.stream()
                         .peek(cdmip -> {
+                            if (cdmip.sample.slideCode.equals("")) {
+                                LOG.info("!!!! GOT {}", cdmip.sample.slideCode);
+                            }
                             if (Files.notExists(Paths.get(cdmip.filepath))) {
                                 LOG.warn("No filepath {} found for {} (sample: {}, publishedName: {})", cdmip.filepath, cdmip, cdmip.sampleRef, !hasSample(cdmip) ? "no sample": cdmip.sample.publishingName);
                             }
