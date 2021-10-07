f22fe6faaf7dfcda1bdd609272b7e3e545820edf
Cristian Goina <goinac@hhmi.org>
2021-10-07 19:08:23 -0400

    finished the unit test for color depth search

--- colormipsearch-api/src/test/java/org/janelia/colormipsearch/api/cdsearch/PixelMatchColorDepthSearchAlgorithmTest.java
+++ colormipsearch-api/src/test/java/org/janelia/colormipsearch/api/cdsearch/PixelMatchColorDepthSearchAlgorithmTest.java
@@ -1,10 +1,15 @@
 package org.janelia.colormipsearch.api.cdsearch;
 
+import java.util.Collections;
+
 import ij.ImagePlus;
 import ij.io.Opener;
 import org.janelia.colormipsearch.api.imageprocessing.ImageArray;
 import org.janelia.colormipsearch.api.imageprocessing.ImageArrayUtils;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PixelMatchColorDepthSearchAlgorithmTest {
 
@@ -14,7 +19,22 @@ public class PixelMatchColorDepthSearchAlgorithmTest {
         ImagePlus testTarget = new Opener().openTiff("src/test/resources/colormipsearch/api/cdsearch/GMR_31G04_AE_01-20190813_66_F3-40x-Brain-JRC2018_Unisex_20x_HR-2704505419467849826-CH2-07_CDM.tif", 1);
         ImageArray<?> testMaskArray = ImageArrayUtils.fromImagePlus(testMask);
         ImageArray<?> testTargetArray = ImageArrayUtils.fromImagePlus(testTarget);
-        // TODO - finish it
+        PixelMatchColorDepthSearchAlgorithm colorDepthSearchAlgorithm = new PixelMatchColorDepthSearchAlgorithm(
+            testMaskArray,
+            20,
+            true,
+            null,
+            0,
+            false,
+            20,
+            0.01,
+            2,
+            img -> (x, y) -> x >= img.getWidth() - 260 && y < 90 || x < 330 && y < 100
+        );
+        ColorMIPMatchScore score = colorDepthSearchAlgorithm.calculateMatchingScore(testTargetArray, Collections.emptyMap());
+        assertTrue(score.isMatch());
+        assertEquals(87, score.getMatchingPixNum());
     }
+
 
 }
