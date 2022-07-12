ea62b772756a56bd2683b3969774a66962e1593e
Cristian Goina <goinac@hhmi.org>
2022-07-12 17:16:24 -0400

    added some comments to the mixin class

--- colormipsearch-persist/src/main/java/org/janelia/colormipsearch/dao/mongo/support/AbstractMatchMixIn.java
+++ colormipsearch-persist/src/main/java/org/janelia/colormipsearch/dao/mongo/support/AbstractMatchMixIn.java
@@ -5,6 +5,13 @@ import com.fasterxml.jackson.annotation.JsonFilter;
 import org.janelia.colormipsearch.model.AbstractMatch;
 import org.janelia.colormipsearch.model.AbstractNeuronMetadata;
 
+/**
+ * The purpose of this MixIn is just to use JsonFilter annotation
+ * and it is only used when serializing to Mongo.
+ * So far when we write JSON to the file system we don't want to use any filter.
+ * @param <M> mask image type
+ * @param <T> target image type
+ */
 @JsonFilter("useRefIdFilter")
 public abstract class AbstractMatchMixIn<M extends AbstractNeuronMetadata, T extends AbstractNeuronMetadata> extends AbstractMatch<M, T> {
 }
