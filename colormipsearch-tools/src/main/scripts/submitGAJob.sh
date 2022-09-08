f05c0d37bbb00bb7bc0e3d643094917086b9eb6d
Cristian Goina <goinac@hhmi.org>
2022-09-08 14:54:23 -0400

    removed reverse grad score update as it longer applies; updated submitGA

--- colormipsearch-tools/src/main/scripts/submitGAJob.sh
+++ colormipsearch-tools/src/main/scripts/submitGAJob.sh
@@ -4,9 +4,6 @@ function runGAJob {
     CDGA_INPUT_PARAM=$1
     shift
 
-    CDGA_OUTPUT_PARAM=$1
-    shift
-
     NEGATIVE_RADIUS=20
     MASK_THRESHOLD=20
 
@@ -19,22 +16,6 @@ function runGAJob {
     CONCURRENCY_OPTS=${CONCURRENCY_OPTS:-"--cdsConcurrency ${CONCURRENCY}"}
 
     MEM_OPTS="-Xmx${MEM_RESOURCE}G -Xms${MEM_RESOURCE}G"
-    CDGA_ZGAP_SUFFIX=${CDGA_ZGAP_SUFFIX:_20pxRGBMAX}
-
-    if [ -n "${CDGA_GRADIENTS_LOCATION}" ] ; then
-        CDGA_GRAD_OPTS="-gp ${CDGA_GRADIENTS_LOCATION}"
-    else
-        CDGA_GRAD_OPTS=
-    fi
-
-    if [ -n "${CDGA_ZGAP_LOCATION}" ] ; then
-        CDGA_ZGAP_OPTS="-zgp ${CDGA_ZGAP_LOCATION}"
-        if [ -n "${CDGA_ZGAP_SUFFIX}" ] ; then
-            CDGA_ZGAP_OPTS="${CDGA_ZGAP_OPTS} --zgapSuffix ${CDGA_ZGAP_SUFFIX}"
-        fi
-    else
-        CDGA_ZGAP_OPTS=
-    fi
 
     if [ -n "${LOGCONFIGFILE}" ] && [ -f "${LOGCONFIGFILE}" ] ; then
         echo "Using Log config: ${LOGCONFIGFILE}"
@@ -54,29 +35,23 @@ function runGAJob {
         --maskThreshold ${MASK_THRESHOLD} \
         --negativeRadius ${NEGATIVE_RADIUS} \
         --mirrorMask \
-        --topPublishedNameMatches ${TOP_RESULTS} \
-        --topPublishedSampleMatches ${SAMPLES_PER_LINE} \
+        --nBestLines ${TOP_RESULTS} \
+        --nBestSamplesPerLine ${SAMPLES_PER_LINE} \
         --processingPartitionSize ${PROCESSING_PARTITION_SIZE} \
-        ${CDGA_GRAD_OPTS} \
-        ${CDGA_ZGAP_OPTS} \
         ${CONCURRENCY_OPTS} \
-        -rd ${CDGA_INPUT_PARAM} \
-        -od ${CDGA_OUTPUT_PARAM} \
+        -md ${CDGA_INPUT_PARAM} \
         $*"
 
     echo "Running on $HOSTNAME: ${cmd}"
     ($cmd)
 }
 
-CDGA_INPUT_DIR=${CDGA_INPUT_DIR:-$1}
-CDGA_OUTPUT_DIR=${CDGA_OUTPUT_DIR:-$2}
+CDGA_INPUT=${MASKS_LIBRARY:-$1}
 LSB_JOBINDEX=${LSB_JOBINDEX:-$3}
 
 JOB_INDEX=$((LSB_JOBINDEX - 1))
-JOB_START_FILE_INDEX=$((JOB_INDEX * FILES_PER_JOB + START_FILE_INDEX))
+JOB_START_MIP_INDEX=$((JOB_INDEX * MIP_IDS_PER_JOB + START_MIP_ID_INDEX))
 
 JOB_LOGPREFIX=${JOB_LOGPREFIX:-}
-echo "$(date) Gradient Adjustment Job $LSB_JOBINDEX: $INPUT_INDEX"
-runGAJob \
-  "${CDGA_INPUT_DIR}:${JOB_START_FILE_INDEX}:${FILES_PER_JOB}" \
-  "${CDGA_OUTPUT_DIR}" > ${JOB_LOGPREFIX}ga_${LSB_JOBINDEX}_${JOB_START_FILE_INDEX}_${FILES_PER_JOB}.log
+echo "$(date) Gradient Adjustment Job $LSB_JOBINDEX: $JOB_START_MIP_INDEX"
+runGAJob "${CDGA_INPUT}:${JOB_START_MIP_INDEX}:${MIP_IDS_PER_JOB}" > ${JOB_LOGPREFIX}ga_${LSB_JOBINDEX}_${JOB_START_MIP_INDEX}_${MIP_IDS_PER_JOB}.log
