958dd34db9940a6c28280993ab97e755af1bf21c
Cristian Goina <goinac@hhmi.org>
2023-01-09 14:56:13 -0500

    read tree node to prevent an infinite loop

--- colormipsearch-api/src/main/java/org/janelia/colormipsearch/model/json/FileDataDeserializer.java
+++ colormipsearch-api/src/main/java/org/janelia/colormipsearch/model/json/FileDataDeserializer.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import org.janelia.colormipsearch.model.FileData;
 
@@ -17,7 +18,15 @@ public class FileDataDeserializer extends JsonDeserializer<FileData> {
         } else if (p.getCurrentToken() == JsonToken.VALUE_STRING) {
             return FileData.fromString(p.getValueAsString());
         } else if (p.getCurrentToken() == JsonToken.START_OBJECT) {
-            return p.readValueAs(FileData.class);
+            JsonNode n = p.readValueAsTree();
+            JsonNode dataTypeNode = n.findValue("dataType");
+            JsonNode fileNameNode = n.findValue("fileName");
+            JsonNode entryNameNode = n.findValue("entryName");
+            return FileData.fromComponents(
+                    dataTypeNode != null ? FileData.FileDataType.valueOf(dataTypeNode.asText()) : FileData.FileDataType.file,
+                    fileNameNode != null ? fileNameNode.asText() : null,
+                    entryNameNode != null ? entryNameNode.asText() : null
+            );
         } else {
             return null;
         }
