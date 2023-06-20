b553d231ba189e2a6300228a4525fc6716014b32
Cristian Goina <goinac@hhmi.org>
2023-06-20 14:52:19 -0400

    temporary no type info for cdmatch

--- colormipsearch-api/src/main/java/org/janelia/colormipsearch/model/CDMatchEntity.java
+++ colormipsearch-api/src/main/java/org/janelia/colormipsearch/model/CDMatchEntity.java
@@ -1,6 +1,7 @@
 package org.janelia.colormipsearch.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import org.apache.commons.lang3.StringUtils;
 import org.janelia.colormipsearch.cds.GradientAreaGapUtils;
@@ -8,6 +9,7 @@ import org.janelia.colormipsearch.dto.AbstractNeuronMetadata;
 import org.janelia.colormipsearch.dto.CDMatchedTarget;
 import org.janelia.colormipsearch.model.annotations.PersistenceInfo;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 @PersistenceInfo(storeName ="cdMatches")
 public class CDMatchEntity<M extends AbstractNeuronEntity, T extends AbstractNeuronEntity> extends AbstractMatchEntity<M, T> {
     private Float normalizedScore;
